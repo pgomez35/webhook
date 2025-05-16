bddf37e9dfa27f3d07d63e0796342cacfaa16e62
pgomez35 <pgomez35@gmail.com>
2025-05-16 17:57:24 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = FastAPI()
 
 @app.get("/webhook")
 def verify_webhook(hub_mode: str = "", hub_verify_token: str = "", hub_challenge: str = ""):
+    print("Modo:", hub_mode)
+    print("Token:", hub_verify_token)
+    print("Challenge:", hub_challenge)
     if hub_mode == "subscribe":
         return PlainTextResponse(hub_challenge)
     return PlainTextResponse("Modo no válido", status_code=400)
+
